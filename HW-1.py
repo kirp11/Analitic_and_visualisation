4a6022a0b7c2648b00627ae53515c52358231890
Ivan K <kosi17100@gmail.com>
2025-08-29 17:55:46 +0300

    second commit

---
+++ HW-1.py
@@ -0,0 +1,6 @@
+import numpy as np
+
+# #1
+temps = np.array([15.2, 16.8, 14.5, 17.0, 16.1])
+
+print(f"cумму {np.sum(temps)}, среднее  {np.mean(temps)}, минимум  {np.min(temps)} и максимум массива  {np.max(temps)}")
